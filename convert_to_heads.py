a68bf316e8f2158b11d15dc6454ad56357a3c94e
Karel D'Oosterlinck <kareldoosterlinck@gmail.com>
2023-10-09 13:57:38 -0700

    select CC as head words for coordinated entities

--- convert_to_heads.py
+++ convert_to_heads.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 import logging
 import os
 from typing import Tuple
+from functools import lru_cache
 
 import jsonlines
 
@@ -11,7 +12,42 @@ FILENAME = "english_{}{}.jsonlines"
 LOGGING_LEVEL = logging.WARNING  # DEBUG to output all duplicate spans
 SPLITS = ("development", "test", "train")
 
+class DynamicDepth():
+    """Implements a cache + dynamic programming to find the relative depth of every word in a subphrase given the head word for every word.
+    """
+    def get_parse_depths(self, heads, start, end):
+        """Return the relative depth for every word
 
+        Args:
+            heads (list): List where each entry is the index of that entry's head word in the dependency parse
+            start (int): starting index of the heads for the subphrase
+            end (int): ending index of the heads for the subphrase
+
+        Returns:
+            list: Relative depth in the dependency parse for every word
+        """
+        self.heads = heads[start:end]
+        self.relative_heads = [h - start if h else -100 for h in self.heads] # -100 to deal with 'none' headwords
+
+        depths = [self._get_depth_recursive(h) for h in range(len(self.relative_heads))]
+
+        return depths
+
+    @lru_cache(maxsize=None)
+    def _get_depth_recursive(self, index):
+        """Recursively get the depths of every index using a cache and recursion
+
+        Args:
+            index (int): Index of the word for which to calculate the relative depth
+
+        Returns:
+            int: Relative depth of the word at the index
+        """
+        # if the head for the current index is outside the scope, this index is a relative root
+        if self.relative_heads[index] >= len(self.relative_heads) or self.relative_heads[index] < 0:
+            return 0
+        return self._get_depth_recursive(self.relative_heads[index]) + 1
+    
 def get_head(mention: Tuple[int, int], doc: dict) -> int:
     """Returns the span's head, which is defined as the only word within the
     span whose head is outside of the span or None. In case there are no or
@@ -26,6 +62,18 @@ def get_head(mention: Tuple[int, int], doc: dict) -> int:
     """
     head_candidates = set()
     start, end = mention
+
+    # use head information to extract parse depth
+    dynamicDepth = DynamicDepth()
+    depth = dynamicDepth.get_parse_depths(doc['head'], start, end)
+    depth_limit = 2
+
+    # return first 'CC' token above depth limit, if exists
+    cc_indexes = [i for i in range(end - start) if doc['pos'][i+start] == 'CC' and depth[i] < depth_limit]
+    if cc_indexes:
+        return cc_indexes[0] + start
+
+    # else return syntactic head
     for i in range(start, end):
         ith_head = doc["head"][i]
         if ith_head is None or not (start <= ith_head < end):
